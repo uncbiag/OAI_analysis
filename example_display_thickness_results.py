90c21bac4b31f00ebf5eba78cc1ae2232d9b58cc
mn <mn@cs.unc.edu>
2019-07-16 12:37:40 +0200

    Improved example visualziation code.

--- example_display_thickness_results.py
+++ example_display_thickness_results.py
@@ -6,11 +6,16 @@ def create_visualizations(dd,thickness,cartilage_type,timepoint,output_prefix):
     td = dd.loc[(dd['cartilage_type']==cartilage_type) & (dd['timepoint']==timepoint)]
     ids = td['cartilage_type_id'].to_numpy(dtype=int)
 
+    print('INFO: {} measurements for timepoint {} of {}'.format(len(ids), timepoint, cartilage_type))
+
     thickness_selected = thickness[ids, ...]
 
     # now compute the statistical measures ignoring NaNs
     mean_val = np.nanmean(thickness_selected, axis=0)
     std_val = np.nanstd(thickness_selected, axis=0)
+
+    number_of_non_nan_measurements = np.sum(~np.isnan(thickness_selected) &
+                                            (np.greater(thickness_selected,0.0, where=~np.isnan(thickness_selected))),axis=0)
 
     output_all_prefix = output_prefix + '_' + cartilage_type + '_' + timepoint
 
@@ -25,6 +30,12 @@ def create_visualizations(dd,thickness,cartilage_type,timepoint,output_prefix):
     plt.colorbar()
     plt.title('std' + ' ' + timepoint )
     plt.savefig( output_all_prefix + '_std.pdf')
+
+    plt.clf()
+    plt.imshow(number_of_non_nan_measurements)
+    plt.colorbar()
+    plt.title('numer of non-zero measurements' + ' ' + timepoint)
+    plt.savefig( output_all_prefix + '_non_zero.pdf')
 
 
 femoral_thickness = np.load('thickness_results_femoral_cartilage.npz', allow_pickle=True)['data']
