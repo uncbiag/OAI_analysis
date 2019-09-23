2ac2102e5b961d2f93c31751a7001a24fc46760a
Zhenlin Xu <zhenlinx@biag-w01.cs.unc.edu>
2019-09-22 23:58:22 -0400

    parsing pid from file

--- misc/str_ops.py
+++ misc/str_ops.py
@@ -4,8 +4,6 @@ Operations on strings
 Created by zhenlinx on 1/19/19
 """
 
-
-
 def replace_extension(name: str, string: str):
     """
     Replace the file extension with a string
@@ -15,3 +13,24 @@ def replace_extension(name: str, string: str):
     """
     preflix = '.'.join(name.split('.')[:-1])
     return "{}{}".format(preflix, string)
+
+
+def read_pid_from_file(pid_file):
+    """
+    get pid from a text file in format
+    ```
+    index PID
+    1 XXXX
+    2 XXXX
+    3 XXXX
+    ```
+    :param pid_file:
+    :return:
+    """
+    pids = []
+    with open(pid_file) as f:
+        lines = f.readlines()
+        for l in lines[1:]:
+            pids.append(int(l.strip().split(' ')[1]))
+
+    return pids
