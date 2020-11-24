42e3bc14e8bf0c60bb5f71689dd66c780680bcf4
AWS ParallelCluster user <centos@ip-10-0-1-174.ec2.internal>
2020-11-24 08:43:49 +0000

    minor fixes

--- setup.sh
+++ setup.sh
@@ -26,6 +26,25 @@ python3.6 -m pip install pandas torch torchvision &>/dev/null
 
 sudo yum install eigen3-devel gmp-devel gmpxx4ldbl mpfr-devel boost-devel boost-thread-devel tbb-devel python3-devel scotch-devel cmake3 &>/dev/null
 
+
+#installing the above packages on the compute nodes
+sinfo_out=$(sinfo list | awk 'NF>1{print $NF}' | sed '1d' )
+
+compute_prefix=$(echo $sinfo_out | cut -d[ -f1)
+
+compute_range=$( echo "$sinfo_out" | cut -d "[" -f2- | cut -d "]" -f1 )
+
+compute_start=$(echo "$compute_range" | cut -d\- -f1)
+
+compute_end=$(echo "$compute_range" | cut -d\- -f2)
+
+echo ${compute_prefix}
+
+for ((i=compute_start;i<=compute_end;i++)); do
+ echo "installing packages on ${compute_prefix}${i}"
+ ssh ${compute_prefix}${i} sudo yum install eigen3-devel gmp-devel gmpxx4ldbl mpfr-devel boost-devel boost-thread-devel tbb-devel python3-devel scotch-devel cmake3 &>/dev/null
+done
+
 sudo yum install libmpc-devel &>/dev/null
 sudo yum install gcc-c++
 echo 'dependency module installation complete'
@@ -46,11 +65,14 @@ sudo ln -sf /usr/bin/cmake3 /usr/bin/cmake &>/dev/null
 
 cd ..
 
+echo 'installing PyMesh'
+
 git clone https://github.com/PyMesh/PyMesh.git &>/dev/null
 git checkout e3c777a66c92f97dcfea610f66bbffa60701cd5f -b test &>/dev/null
 git submodule update --init &>/dev/null
 pip3.6 install -r python/requirements.txt &>/dev/null
 #edit setup.py: #!/usr/bin/env python3.6
+sed -i 's/#!\/usr\/bin\/env\ python/#!\/usr\/bin\/env\ python3.6/g' setup.py
 cd third_party
 mkdir build
 cd build
@@ -71,10 +93,23 @@ cd ..
 python3.6 ./setup.py install &>/dev/null
 export LD_LIBRARY_PATH=/shared/lib64
 
+echo 'PyMesh installation complete'
+
+echo 'Installing surface-distance'
+
 cd ../
 git clone https://github.com/deepmind/surface-distance &>/dev/null
 pip3.6 install surface-distance/
 
-#TODO: add file editings for PyMesh
-#TODO: niftyreg
-#TODO: data download
+echo 'surface-distance installation complete'
+
+echo 'installing niftyreg'
+git clone git://git.code.sf.net/p/niftyreg/git niftyreg-git
+cd niftyreg-git
+mkdir build
+mkdir install
+cd build
+cmake3 -D CMAKE_C_COMPILER=/shared/bin/gcc -D CMAKE_CXX_COMPILER=/shared/bin/g++ -D CMAKE_INSTALL_PREFIX=../install/
+make install
+
+echo 'niftyreg installation complete'
